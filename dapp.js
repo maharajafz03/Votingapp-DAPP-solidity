629324d22ae3f71ff6414bf38bbd8b4a1f281830
maharajafz03 <maharajafz1999@gmail.com>
2024-06-17 20:41:46 +0530

    [meta mask not opening]

--- dapp.js
+++ dapp.js
@@ -1,21 +1,16 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     if (window.ethereum) {
-//         ethereum.request({ method: "eth_requestAccounts" })
-//         .then((accounts) => console.log(accounts))
-//         .catch((error) => console.error(error));
-//     }
-// });
+
+//ERROR [meta mask not opening..]
 
 document.addEventListener("DOMContentLoaded", function() {
-    if (window.ethereum) {
-        ethereum.request({ method: "eth_requestAccounts" })
-        .then((accounts) => {
-            console.log('Accounts:', accounts);
-        })
-        .catch((error) => {
-            console.error('Error requesting accounts:', error);
+            if (window.ethereum) {
+                ethereum.request({ method: "eth_requestAccounts" })
+                .then((accounts) => {
+                    console.log('Accounts:', accounts);
+                })
+                .catch((error) => {
+                    console.error('Error requesting accounts:', error);
+                });
+            } else {
+                console.error('MetaMask is not installed');
+            }
         });
-    } else {
-        console.error('MetaMask is not installed');
-    }
-});
